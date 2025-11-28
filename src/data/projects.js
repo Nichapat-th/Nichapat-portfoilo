cdc99a80305ea0f49a72b8e04b8ff369500a7902
Nichapat-th <74952396+Nichapat-th@users.noreply.github.com>
2025-11-28 16:32:18 +0700

    feat: update image

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,6 +7,10 @@ import piika3 from '../assets/piika_3.png';
 import led1 from '../assets/led_1.png';
 import led2 from '../assets/led_2.png';
 import led3 from '../assets/led_3.png';
+import lucky1 from '../assets/lucky_1.jpg';
+import whattoeat1 from '../assets/whattoeat_1.png';
+import whattoeat2 from '../assets/whattoeat_2.png';
+import whattoeat3 from '../assets/whattoeat_3.png';
 
 export const projects = [
   {
@@ -37,20 +41,22 @@ export const projects = [
     images: [led1, led2, led3],
   },
   {
-    id: 'lucky-color',
-    title: 'Lucky Color',
-    subtitle: 'Motelu',
-    tags: ['Delivery', 'Hosting', 'Forum'],
-    category: 'Mobile App',
-    description: 'Color-based mobile application',
-  },
-  {
     id: 'what-to-eat',
     title: 'What To Eat ?',
     subtitle: 'Random Menu Design',
     tags: ['Delivery', 'Hosting', 'Forum'],
     category: 'Mobile App',
     description: 'Random menu selection app to help decide what to eat',
+    images: [whattoeat1, whattoeat2, whattoeat3],
+  },
+  {
+    id: 'lucky-color',
+    title: 'Lucky Color',
+    subtitle: 'Motelu',
+    tags: ['Delivery', 'Hosting', 'Forum'],
+    category: 'Mobile App',
+    description: 'Color-based mobile application',
+    images: [lucky1],
   },
 ];
 
